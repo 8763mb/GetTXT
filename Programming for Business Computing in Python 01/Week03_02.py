774e733c37f0bf8807ec449a765a9713c699e438
Huang <bobohuang@fareastone.com.tw>
2023-05-03 11:53:22 +0800

    fix;

--- Programming for Business Computing in Python 01/Week03_02.py
+++ Programming for Business Computing in Python 01/Week03_02.py
@@ -7,45 +7,18 @@ E = (A - B * 500 - C * 100 - D * 50) // 10
 F = (A - B * 500 - C * 100 - D * 50 - E * 10) // 5
 G = (A - B * 500 - C * 100 - D * 50 - E * 10 - F * 5)
 
-if B < 1:
-  B = None
-else:
-  B = '500, ' + str(B)
-
-if C < 1:
-  C = None
-else:
-  C = '100, ' + str(C)
-
-if D < 1:
-  D = None
-else:
-  D = '50, ' + str(D)
-
-if E < 1:
-  E = None
-else:
-  E = '10, ' + str(E)
-
-if F < 1:
-  F = None
-else:
-  F = '5, ' + str(F)
-
-if G < 1:
-  G = None
-else:
-  G = '5, ' + str(G)
-
-if B is not None:
-  print(B, end=';')
-if C is not None:
-  print(C, end=';')
-if D is not None:
-  print(D, end=';')
-if E is not None:
-  print(E, end=';')
-if F is not None:
-  print(F, end=';')
-if G is not None:
-  print(G)
+output_list = []
+if B > 0:
+    output_list.append(f"500, {B}")
+if C > 0:
+    output_list.append(f"100, {C}")
+if D > 0:
+    output_list.append(f"50, {D}")
+if E > 0:
+    output_list.append(f"10, {E}")
+if F > 0:
+    output_list.append(f"5, {F}")
+if G > 0:
+    output_list.append(f"1, {G}")
+    
+print(';'.join(output_list))
