b683c9981825a0a2a817027e2c32fcb97d41f95d
Huang <bobohuang@fareastone.com.tw>
2023-05-10 15:33:10 +0800

    Update comment

--- Programming for Business Computing in Python 01/Week04_01.py
+++ Programming for Business Computing in Python 01/Week04_01.py
@@ -1,22 +1,27 @@
-cost = int(input())      
-price = int(input())     
-maxDemand = int(input()) #固定是8
-quantity = int(input())
+# 讀取輸入資料
+cost = int(input())      # 每單位進貨成本
+price = int(input())     # 每單位零售價格
+maxDemand = int(input()) # 固定8
+quantity = int(input())  # 訂購數量
 
-
+# 初始化預期銷售為 0
 expSales = 0.0
 
 
-for demand in range(maxDemand + 1):  #跑8次
-  prob = float(input())              #輸入8次機率
+for demand in range(maxDemand + 1):  #跑9次
+    
+    prob = float(input())              
 
-  if demand < quantity:         
-    expSales += prob * demand        #計算獲利
-  else:                        
-    expSales += prob * quantity
+    
+    if demand < quantity:         
+        # 將需求量乘以概率加入預期銷售
+        expSales += prob * demand       
+    else:                        
+        # 將訂購量乘以概率加入預期銷售
+        expSales += prob * quantity
 
-# calculate and print the result
 print(expSales)
-profit = expSales * price - quantity * cost  #獲利減成本
-print(int(profit))
 
+# 計算利潤，並輸出整數部分
+profit = expSales * price - quantity * cost  
+print(int(profit))
